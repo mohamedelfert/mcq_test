4b62ae65213a3410ed47569ac472c9dcbfca2731
mohamedelfert <me_me4152@yahoo.com>
2022-04-27 06:10:00 +0200

    last database.

--- database/quiz_mcq.sql
+++ database/quiz_mcq.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 25, 2022 at 11:39 PM
+-- Generation Time: Apr 27, 2022 at 06:04 AM
 -- Server version: 10.1.32-MariaDB
 -- PHP Version: 7.2.5
 
@@ -56,13 +56,13 @@ CREATE TABLE `migrations` (
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (2, '2014_10_12_100000_create_password_resets_table', 1),
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2022_04_22_185351_create_permission_tables', 1),
 (7, '2022_04_23_191328_create_topics_table', 2),
 (8, '2022_04_24_192037_create_questions_table', 2),
 (9, '2022_04_24_192125_create_question_options_table', 2),
-(10, '2014_10_12_000000_create_users_table', 3),
 (11, '2022_04_25_110359_create_tests_table', 3),
-(12, '2022_04_25_110414_create_results_table', 3);
+(12, '2022_04_25_110414_create_results_table', 3),
+(13, '2014_10_12_000000_create_users_table', 4),
+(14, '2022_04_22_185351_create_permission_tables', 4);
 
 -- --------------------------------------------------------
 
@@ -94,8 +94,11 @@ CREATE TABLE `model_has_roles` (
 
 INSERT INTO `model_has_roles` (`role_id`, `model_type`, `model_id`) VALUES
 (1, 'App\\User', 1),
-(2, 'App\\User', 2),
-(2, 'App\\User', 3);
+(2, 'App\\User', 3),
+(2, 'App\\User', 4),
+(2, 'App\\User', 5),
+(2, 'App\\User', 6),
+(2, 'App\\User', 7);
 
 -- --------------------------------------------------------
 
@@ -130,41 +133,41 @@ CREATE TABLE `permissions` (
 --
 
 INSERT INTO `permissions` (`id`, `name`, `display_name`, `guard_name`, `created_at`, `updated_at`, `routes`) VALUES
-(1, 'user-list', 'المستخدمين', 'web', '2022-04-22 19:19:47', '2022-04-22 19:19:47', 'users.index'),
-(2, 'user-create', 'المستخدمين', 'web', '2022-04-22 19:19:47', '2022-04-22 19:19:47', 'users.create,users.store'),
-(3, 'user-edit', 'المستخدمين', 'web', '2022-04-22 19:19:47', '2022-04-22 19:19:47', 'users.edit,users.update'),
-(4, 'user-delete', 'المستخدمين', 'web', '2022-04-22 19:19:47', '2022-04-22 19:19:47', 'users.destroy'),
-(5, 'role-list', 'الصلاحيات', 'web', '2022-04-22 19:19:47', '2022-04-22 19:19:47', 'roles.index'),
-(6, 'role-create', 'الصلاحيات', 'web', '2022-04-22 19:19:47', '2022-04-22 19:19:47', 'roles.create,roles.store'),
-(7, 'role-edit', 'الصلاحيات', 'web', '2022-04-22 19:19:47', '2022-04-22 19:19:47', 'roles.edit,roles.update'),
-(8, 'role-delete', 'الصلاحيات', 'web', '2022-04-22 19:19:47', '2022-04-22 19:19:47', 'roles.destroy'),
-(9, 'language', 'اللغه', 'web', '2022-04-21 22:00:00', '2022-04-21 22:00:00', 'lang'),
-(10, 'topic-list', 'المواضيع', 'web', '2022-04-22 22:00:00', '2022-04-22 22:00:00', 'topics.index'),
-(11, 'topic-create', 'المواضيع', 'web', '2022-04-22 22:00:00', '2022-04-22 22:00:00', 'topics.create,topics.store'),
-(12, 'topic-edit', 'المواضيع', 'web', '2022-04-22 22:00:00', '2022-04-22 22:00:00', 'topics.edit,topics.update'),
-(13, 'topic-delete', 'المواضيع', 'web', '2022-04-22 22:00:00', '2022-04-22 22:00:00', 'topics.destroy'),
-(14, 'user-show', 'المستخدمين', 'web', '2022-04-22 22:00:00', '2022-04-22 22:00:00', 'users.show'),
-(15, 'role-show', 'الصلاحيات', 'web', '2022-04-22 22:00:00', '2022-04-22 22:00:00', 'roles.show'),
-(16, 'question-list', 'الاسئله', 'web', '2022-04-23 22:00:00', '2022-04-23 22:00:00', 'questions.index'),
-(17, 'question-create', 'الاسئله', 'web', '2022-04-23 22:00:00', '2022-04-23 22:00:00', 'questions.create,questions.store'),
-(18, 'question-edit', 'الاسئله', 'web', '2022-04-23 22:00:00', '2022-04-23 22:00:00', 'questions.edit,questions.update'),
-(19, 'question-delete', 'الاسئله', 'web', '2022-04-23 22:00:00', '2022-04-23 22:00:00', 'questions.destroy'),
-(20, 'question-show', 'الاسئله', 'web', '2022-04-23 22:00:00', '2022-04-23 22:00:00', 'questions.show'),
-(21, 'option-list', 'الاختيارات', 'web', '2022-04-23 22:00:00', '2022-04-23 22:00:00', 'options.index'),
-(22, 'option-create', 'الاختيارات', 'web', '2022-04-23 22:00:00', '2022-04-23 22:00:00', 'options.create,options.store'),
-(23, 'option-edit', 'الاختيارات', 'web', '2022-04-23 22:00:00', '2022-04-23 22:00:00', 'options.edit,options.update'),
-(24, 'option-delete', 'الاختيارات', 'web', '2022-04-23 22:00:00', '2022-04-23 22:00:00', 'options.destroy'),
-(25, 'option-show', 'الاختيارات', 'web', '2022-04-23 22:00:00', '2022-04-23 22:00:00', 'options.show'),
-(26, 'test-list', 'الامتحانات', 'web', '2022-04-24 22:00:00', '2022-04-24 22:00:00', 'tests.index'),
-(27, 'test-create', 'الاختبارات', 'web', '2022-04-24 22:00:00', '2022-04-24 22:00:00', 'tests.create,tests.store'),
-(28, 'test-edit', 'الاختبارات', 'web', '2022-04-24 22:00:00', '2022-04-24 22:00:00', 'tests.create,tests.store'),
-(29, 'test-delete', 'الاختبارات', 'web', '2022-04-24 22:00:00', '2022-04-24 22:00:00', 'tests.destroy'),
-(30, 'test-show', 'الاختبارات', 'web', '2022-04-24 22:00:00', '2022-04-24 22:00:00', 'tests.show'),
-(31, 'result-list', 'النتائج', 'web', '2022-04-24 22:00:00', '2022-04-24 22:00:00', 'results.index'),
-(32, 'result-create', 'النتائج', 'web', '2022-04-24 22:00:00', '2022-04-24 22:00:00', 'results.create,results.store'),
-(33, 'result-edit', 'النتائج', 'web', '2022-04-24 22:00:00', '2022-04-24 22:00:00', 'results.edit,results.update'),
-(34, 'result-delete', 'النتائج', 'web', '2022-04-24 22:00:00', '2022-04-24 22:00:00', 'results.destroy'),
-(35, 'result-show', 'النتائج', 'web', '2022-04-24 22:00:00', '2022-04-24 22:00:00', 'results.show');
+(1, 'user-list', 'المستخدمين', 'web', NULL, NULL, 'users.index'),
+(2, 'user-create', 'المستخدمين', 'web', NULL, NULL, 'users.create,users.store'),
+(3, 'user-edit', 'المستخدمين', 'web', NULL, NULL, 'users.edit,users.update'),
+(4, 'user-delete', 'المستخدمين', 'web', NULL, NULL, 'users.destroy'),
+(5, 'role-list', 'الصلاحيات', 'web', NULL, NULL, 'roles.index'),
+(6, 'role-create', 'الصلاحيات', 'web', NULL, NULL, 'roles.create,roles.store'),
+(7, 'role-edit', 'الصلاحيات', 'web', NULL, NULL, 'roles.edit,roles.update'),
+(8, 'role-delete', 'الصلاحيات', 'web', NULL, NULL, 'roles.destroy'),
+(9, 'language', 'اللغه', 'web', NULL, NULL, 'lang'),
+(10, 'topic-list', 'المواضيع', 'web', NULL, NULL, 'topics.index'),
+(11, 'topic-create', 'المواضيع', 'web', NULL, NULL, 'topics.create,topics.store'),
+(12, 'topic-edit', 'المواضيع', 'web', NULL, NULL, 'topics.edit,topics.update'),
+(13, 'topic-delete', 'المواضيع', 'web', NULL, NULL, 'topics.destroy'),
+(14, 'user-show', 'المستخدمين', 'web', NULL, NULL, 'users.show'),
+(15, 'role-show', 'الصلاحيات', 'web', NULL, NULL, 'roles.show'),
+(16, 'question-list', 'الاسئله', 'web', NULL, NULL, 'questions.index'),
+(17, 'question-create', 'الاسئله', 'web', NULL, NULL, 'questions.create,questions.store'),
+(18, 'question-edit', 'الاسئله', 'web', NULL, NULL, 'questions.edit,questions.update'),
+(19, 'question-delete', 'الاسئله', 'web', NULL, NULL, 'questions.destroy'),
+(20, 'question-show', 'الاسئله', 'web', NULL, NULL, 'questions.show'),
+(21, 'option-list', 'الاختيارات', 'web', NULL, NULL, 'options.index'),
+(22, 'option-create', 'الاختيارات', 'web', NULL, NULL, 'options.create,options.store'),
+(23, 'option-edit', 'الاختيارات', 'web', NULL, NULL, 'options.edit,options.update'),
+(24, 'option-delete', 'الاختيارات', 'web', NULL, NULL, 'options.destroy'),
+(25, 'option-show', 'الاختيارات', 'web', NULL, NULL, 'options.show'),
+(26, 'test-list', 'الامتحانات', 'web', NULL, NULL, 'tests.index'),
+(27, 'test-create', 'الامتحانات', 'web', NULL, NULL, 'tests.create,tests.store'),
+(28, 'test-edit', 'الامتحانات', 'web', NULL, NULL, 'tests.edit,tests.update'),
+(29, 'test-delete', 'الامتحانات', 'web', NULL, NULL, 'tests.destroy'),
+(30, 'test-show', 'الامتحانات', 'web', NULL, NULL, 'tests.show'),
+(31, 'result-list', 'النتائج', 'web', NULL, NULL, 'results.index'),
+(32, 'result-create', 'النتائج', 'web', NULL, NULL, 'results.create,results.store'),
+(33, 'result-edit', 'النتائج', 'web', NULL, NULL, 'results.edit,results.update'),
+(34, 'result-delete', 'النتائج', 'web', NULL, NULL, 'results.destroy'),
+(35, 'result-show', 'النتائج', 'web', NULL, NULL, 'results.show');
 
 -- --------------------------------------------------------
 
@@ -281,26 +284,60 @@ CREATE TABLE `results` (
 --
 
 INSERT INTO `results` (`id`, `user_id`, `test_id`, `question_id`, `option_id`, `point`, `created_at`, `updated_at`) VALUES
-(1, 2, 1, 4, 16, 1, '2022-04-25 19:37:14', '2022-04-25 19:37:14'),
-(2, 2, 1, 9, 42, 1, '2022-04-25 19:37:14', '2022-04-25 19:37:14'),
-(3, 2, 1, 1, 3, 1, '2022-04-25 19:37:14', '2022-04-25 19:37:14'),
-(4, 2, 1, 5, 21, 1, '2022-04-25 19:37:14', '2022-04-25 19:37:14'),
-(5, 2, 1, 6, 27, 1, '2022-04-25 19:37:14', '2022-04-25 19:37:14'),
-(6, 2, 1, 3, 12, 1, '2022-04-25 19:37:14', '2022-04-25 19:37:14'),
-(7, 2, 1, 10, 46, 1, '2022-04-25 19:37:14', '2022-04-25 19:37:14'),
-(8, 2, 1, 2, 9, 1, '2022-04-25 19:37:14', '2022-04-25 19:37:14'),
-(9, 2, 1, 8, 39, 1, '2022-04-25 19:37:14', '2022-04-25 19:37:14'),
-(10, 2, 1, 7, 33, 1, '2022-04-25 19:37:14', '2022-04-25 19:37:14'),
-(11, 2, 2, 7, 33, 1, '2022-04-25 19:38:18', '2022-04-25 19:38:18'),
-(12, 2, 2, 4, 18, 0, '2022-04-25 19:38:18', '2022-04-25 19:38:18'),
-(13, 2, 2, 2, 9, 1, '2022-04-25 19:38:18', '2022-04-25 19:38:18'),
-(14, 2, 2, 10, 46, 1, '2022-04-25 19:38:18', '2022-04-25 19:38:18'),
-(15, 2, 2, 6, 27, 1, '2022-04-25 19:38:18', '2022-04-25 19:38:18'),
-(16, 2, 2, 1, 3, 1, '2022-04-25 19:38:18', '2022-04-25 19:38:18'),
-(17, 2, 2, 5, 22, 0, '2022-04-25 19:38:18', '2022-04-25 19:38:18'),
-(18, 2, 2, 8, 39, 1, '2022-04-25 19:38:18', '2022-04-25 19:38:18'),
-(19, 2, 2, 3, 12, 1, '2022-04-25 19:38:18', '2022-04-25 19:38:18'),
-(20, 2, 2, 9, 42, 1, '2022-04-25 19:38:18', '2022-04-25 19:38:18');
+(1, 3, 2, 3, 14, 0, '2022-04-26 19:10:06', '2022-04-26 19:10:06'),
+(2, 3, 2, 2, 7, 0, '2022-04-26 19:10:06', '2022-04-26 19:10:06'),
+(3, 3, 2, 7, 31, 0, '2022-04-26 19:10:06', '2022-04-26 19:10:06'),
+(4, 3, 2, 4, 18, 0, '2022-04-26 19:10:06', '2022-04-26 19:10:06'),
+(5, 3, 2, 1, 3, 1, '2022-04-26 19:10:06', '2022-04-26 19:10:06'),
+(6, 3, 2, 9, 41, 0, '2022-04-26 19:10:06', '2022-04-26 19:10:06'),
+(7, 3, 2, 10, 48, 0, '2022-04-26 19:10:06', '2022-04-26 19:10:06'),
+(8, 3, 2, 6, 28, 0, '2022-04-26 19:10:06', '2022-04-26 19:10:06'),
+(9, 3, 2, 5, 21, 1, '2022-04-26 19:10:06', '2022-04-26 19:10:06'),
+(10, 3, 2, 8, 36, 0, '2022-04-26 19:10:06', '2022-04-26 19:10:06'),
+(11, 1, 6, 6, 29, 0, '2022-04-26 19:42:49', '2022-04-26 19:42:49'),
+(12, 1, 6, 3, 14, 0, '2022-04-26 19:42:49', '2022-04-26 19:42:49'),
+(13, 1, 6, 1, 1, 0, '2022-04-26 19:42:49', '2022-04-26 19:42:49'),
+(14, 1, 6, 9, 43, 0, '2022-04-26 19:42:49', '2022-04-26 19:42:49'),
+(15, 1, 6, 5, 21, 1, '2022-04-26 19:42:49', '2022-04-26 19:42:49'),
+(16, 1, 6, 2, 9, 1, '2022-04-26 19:42:49', '2022-04-26 19:42:49'),
+(17, 1, 6, 8, 36, 0, '2022-04-26 19:42:49', '2022-04-26 19:42:49'),
+(18, 1, 6, 7, 31, 0, '2022-04-26 19:42:49', '2022-04-26 19:42:49'),
+(19, 1, 6, 4, 17, 0, '2022-04-26 19:42:49', '2022-04-26 19:42:49'),
+(20, 1, 6, 10, 48, 0, '2022-04-26 19:42:49', '2022-04-26 19:42:49'),
+(21, 3, 7, 7, 32, 0, '2022-04-26 19:43:36', '2022-04-26 19:43:36'),
+(22, 3, 8, 10, 47, 0, '2022-04-26 19:48:37', '2022-04-26 19:48:37'),
+(23, 3, 8, 9, 41, 0, '2022-04-26 19:48:37', '2022-04-26 19:48:37'),
+(24, 3, 9, 6, 27, 1, '2022-04-26 19:50:27', '2022-04-26 19:50:27'),
+(25, 5, 10, 1, 3, 1, '2022-04-26 21:27:24', '2022-04-26 21:27:24'),
+(26, 5, 10, 3, 12, 1, '2022-04-26 21:27:24', '2022-04-26 21:27:24'),
+(27, 5, 10, 6, 27, 1, '2022-04-26 21:27:24', '2022-04-26 21:27:24'),
+(28, 5, 10, 9, 42, 1, '2022-04-26 21:27:24', '2022-04-26 21:27:24'),
+(29, 5, 10, 7, 33, 1, '2022-04-26 21:27:24', '2022-04-26 21:27:24'),
+(30, 5, 10, 5, 21, 1, '2022-04-26 21:27:24', '2022-04-26 21:27:24'),
+(31, 5, 10, 2, 9, 1, '2022-04-26 21:27:24', '2022-04-26 21:27:24'),
+(32, 5, 10, 10, 46, 1, '2022-04-26 21:27:24', '2022-04-26 21:27:24'),
+(33, 5, 10, 4, 16, 1, '2022-04-26 21:27:24', '2022-04-26 21:27:24'),
+(34, 5, 10, 8, 39, 1, '2022-04-26 21:27:24', '2022-04-26 21:27:24'),
+(35, 6, 11, 4, 18, 0, '2022-04-26 21:29:25', '2022-04-26 21:29:25'),
+(36, 6, 11, 10, 46, 1, '2022-04-26 21:29:25', '2022-04-26 21:29:25'),
+(37, 6, 11, 6, 28, 0, '2022-04-26 21:29:25', '2022-04-26 21:29:25'),
+(38, 6, 11, 2, 6, 0, '2022-04-26 21:29:25', '2022-04-26 21:29:25'),
+(39, 6, 11, 8, 38, 0, '2022-04-26 21:29:25', '2022-04-26 21:29:25'),
+(40, 6, 11, 9, 42, 1, '2022-04-26 21:29:25', '2022-04-26 21:29:25'),
+(41, 6, 11, 7, 33, 1, '2022-04-26 21:29:25', '2022-04-26 21:29:25'),
+(42, 6, 11, 1, 3, 1, '2022-04-26 21:29:25', '2022-04-26 21:29:25'),
+(43, 6, 11, 5, 21, 1, '2022-04-26 21:29:25', '2022-04-26 21:29:25'),
+(44, 6, 11, 3, 12, 1, '2022-04-26 21:29:25', '2022-04-26 21:29:25'),
+(45, 7, 12, 1, 3, 1, '2022-04-27 00:52:26', '2022-04-27 00:52:26'),
+(46, 7, 12, 10, 47, 0, '2022-04-27 00:52:26', '2022-04-27 00:52:26'),
+(47, 7, 12, 3, 13, 0, '2022-04-27 00:52:26', '2022-04-27 00:52:26'),
+(48, 7, 12, 7, 31, 0, '2022-04-27 00:52:26', '2022-04-27 00:52:26'),
+(49, 7, 12, 2, 6, 0, '2022-04-27 00:52:26', '2022-04-27 00:52:26'),
+(50, 7, 12, 8, 37, 0, '2022-04-27 00:52:26', '2022-04-27 00:52:26'),
+(51, 7, 12, 9, 44, 0, '2022-04-27 00:52:26', '2022-04-27 00:52:26'),
+(52, 7, 12, 6, 27, 1, '2022-04-27 00:52:26', '2022-04-27 00:52:26'),
+(53, 7, 12, 5, 24, 0, '2022-04-27 00:52:26', '2022-04-27 00:52:26'),
+(54, 7, 12, 4, 16, 1, '2022-04-27 00:52:26', '2022-04-27 00:52:26');
 
 -- --------------------------------------------------------
 
@@ -322,8 +359,8 @@ CREATE TABLE `roles` (
 --
 
 INSERT INTO `roles` (`id`, `name`, `display_name`, `guard_name`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'مدير', 'web', '2022-04-22 19:19:47', '2022-04-22 19:39:43'),
-(2, 'user', 'مستخدم', 'web', '2022-04-22 19:26:53', '2022-04-22 19:26:53');
+(1, 'admin', 'مدير', 'web', '2022-04-26 18:52:10', '2022-04-26 18:52:10'),
+(2, 'user', 'مستخدم', 'web', '2022-04-26 19:04:29', '2022-04-26 19:04:29');
 
 -- --------------------------------------------------------
 
@@ -407,8 +444,18 @@ CREATE TABLE `tests` (
 --
 
 INSERT INTO `tests` (`id`, `result`, `user_id`, `created_at`, `updated_at`) VALUES
-(1, '10', 2, '2022-04-25 19:37:14', '2022-04-25 19:37:14'),
-(2, '8', 2, '2022-04-25 19:38:18', '2022-04-25 19:38:18');
+(1, '0', 3, '2022-04-26 19:09:27', '2022-04-26 19:09:27'),
+(2, '2', 3, '2022-04-26 19:10:06', '2022-04-26 19:10:06'),
+(3, '0', 1, '2022-04-26 19:35:48', '2022-04-26 19:35:48'),
+(4, '0', 1, '2022-04-26 19:36:52', '2022-04-26 19:36:52'),
+(5, '0', 1, '2022-04-26 19:42:37', '2022-04-26 19:42:37'),
+(6, '2', 1, '2022-04-26 19:42:49', '2022-04-26 19:42:49'),
+(7, '0', 3, '2022-04-26 19:43:36', '2022-04-26 19:43:36'),
+(8, '0', 3, '2022-04-26 19:48:37', '2022-04-26 19:48:37'),
+(9, '0', 3, '2022-04-26 19:50:27', '2022-04-26 19:50:27'),
+(10, '10', 5, '2022-04-26 21:27:24', '2022-04-26 21:27:24'),
+(11, '6', 6, '2022-04-26 21:29:25', '2022-04-26 21:29:25'),
+(12, '3', 7, '2022-04-27 00:52:26', '2022-04-27 00:52:26');
 
 -- --------------------------------------------------------
 
@@ -448,6 +495,8 @@ CREATE TABLE `users` (
   `phone` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `role_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `status` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `provider` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `provider_id` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -457,10 +506,13 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `phone`, `role_name`, `status`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Admin', 'admin@admin.com', NULL, '$2y$10$pHZYOXUpbfq.gwEnZR6s6.KfN8ZbqQyhcjhqGkthnNQ553aVnGZnW', '01153225410', 'admin', 'active', NULL, '2022-04-25 09:13:45', '2022-04-25 09:14:26'),
-(2, 'test user', 'bankblood71@gmail.com', NULL, '$2y$10$/VUZW9pYxF82Nk.rDQA99OX4AnIHHnnMyXQd115ng3UzokZBxvvRW', '01011731930', 'user', 'active', NULL, '2022-04-25 09:15:22', '2022-04-25 09:15:45'),
-(3, 'ali ahmed', 'ali@yahoo.com', NULL, '$2y$10$PdjgvqK/euh6K/XwEz4Fv.GsH0yb0zbfb674L0wQQb7C/z2EabDAu', '12345678900', 'user', 'active', NULL, '2022-04-25 09:16:35', '2022-04-25 09:17:25');
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `phone`, `role_name`, `status`, `provider`, `provider_id`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Mohamed Ibrahiem', 'mohamed@yahoo.com', NULL, '$2y$10$wx5XAkOyZmkjl2FtVoPxTu4RJgGWv7NcPj8YPN4kznP.clhZAi88O', '01153225410', 'admin', 'active', NULL, NULL, NULL, '2022-04-26 18:52:10', '2022-04-26 18:52:10'),
+(3, 'test user', 'bankblood71@gmail.com', NULL, '$2y$10$p711tSC6DVO/52qAloe9j.TKZOw7G95san9P.yFJIqW2z.ucJzRR6', '01022648710', 'user', 'active', NULL, NULL, NULL, '2022-04-26 19:06:31', '2022-04-26 19:06:56'),
+(4, 'ahmed', 'ahmed@yahoo.com', NULL, '$2y$10$ecX9OT14jIZWC1Ho/kJGt.i40lcb1qKTHh3PZXc5EvEYY1pMdUq6W', '01101543201', 'user', 'active', NULL, NULL, NULL, '2022-04-26 21:02:55', '2022-04-26 21:03:23'),
+(5, 'junk dog', 'junkdog08@gmail.com', NULL, '', '01141561354', 'user', 'active', 'google', '113943482383485600241', NULL, '2022-04-26 21:20:03', '2022-04-26 21:26:57'),
+(6, 'Toga reyah', 'togareyah@gmail.com', NULL, '', '01122331540', 'user', 'active', 'google', '100394240223440743411', NULL, '2022-04-26 21:28:03', '2022-04-26 21:29:03'),
+(7, 'تطبيق سفره', 'sofraapp@gmail.com', NULL, '$2y$10$BZvcb/2FsoespvhTT96gl.UT3el.1j.A6XNe8fMqrsqIHpKfA.daa', '01061003405', 'user', 'active', 'google', '103085193950315479386', 'FLMvNVg3dcnfM9oMiY2rB95y8gY6CZL41nyU7q1LOXKT0Gq7gO9gZLOKXzjv', '2022-04-26 23:54:50', '2022-04-27 00:51:07');
 
 --
 -- Indexes for dumped tables
@@ -577,7 +629,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `permissions`
@@ -595,25 +647,25 @@ ALTER TABLE `questions`
 -- AUTO_INCREMENT for table `question_options`
 --
 ALTER TABLE `question_options`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- AUTO_INCREMENT for table `results`
 --
 ALTER TABLE `results`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
 -- AUTO_INCREMENT for table `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tests`
 --
 ALTER TABLE `tests`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `topics`
@@ -625,7 +677,7 @@ ALTER TABLE `topics`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
